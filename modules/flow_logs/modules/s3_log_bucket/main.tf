ecaaa7e321ea369759df4aaa8a626dcb9c3952c2
lancehampton <lance@stigian.com>
2025-01-08 16:39:30 -0500

    fix CKV_AWS_145

--- modules/flow_logs/modules/s3_log_bucket/main.tf
+++ modules/flow_logs/modules/s3_log_bucket/main.tf
@@ -18,8 +18,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "flow_logs" {
   bucket = aws_s3_bucket.flow_logs.bucket
 
   rule {
+    bucket_key_enabled = true
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms"
     }
   }
 }
